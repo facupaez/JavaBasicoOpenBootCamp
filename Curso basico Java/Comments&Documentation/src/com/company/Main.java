e68c0720bfa06d3cd179c1414f82a915a7ee3dd0
facupaez <facu.pz10@gmail.com>
2023-03-30 22:54:05 -0300

    comments & documentation

---
+++ Curso basico Java/Comments&Documentation/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * Aquí podemos documentar la aplicación
+ * Clase principal comentarios y documentación
+ * @author Facu Paez
+ */
+public class Main {
+    /**
+     * @param args argumentos a recibir
+     * @since v1.0
+     */
+    public static void main(String[] args) {
+
+        // este es un comentario
+        System.out.println("Comentar esta linea");
+
+        /* Comentario
+        en
+        bloque
+        */
+
+    }
+}
