42011b164f7212d0d3c1f506dcc95897c7c97519
facupaez <facu.pz10@gmail.com>
2023-03-30 23:09:10 -0300

    variables & data types

---
+++ Basic Java Course/Variables & Data Types/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // tipoDeDato nombreVariable = valorVariable;
+        // tipoDeDato nombreVariable;
+        // nombreVariable = valorVariable;
+
+        // numeros enteros
+        byte number1 = 1; //1 byte
+        short number2 = 2; //2 byte
+        int number3 = 3; //4 byte
+        long number4 = 4; //8 byte
+
+        // numeros decimales/flotantes
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        // character
+        char caracter1 = 'a'; //almacenan solo 1 caracter
+
+        // booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //cadenas de texto
+        String nombre = "Charly";
+        String apellido = "Newman";
+
+        //tipos envoltorio
+        Integer numero = null;
+    }
+}
