83ef8e93b7a02e9912ea6e69b213884079393bc8
facupaez <facu.pz10@gmail.com>
2023-04-02 01:33:59 -0300

    operators

---
+++ Basic Java Course/Operators/src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Operadores
+        // aritmeticas
+        /*
+         suma +
+         resta -
+         multiplicacion *
+         division /
+         resto %
+         */
+        int number1 = 2;
+        int number2 = 1;
+
+        int suma = number1 + number2;
+        int resta = number1 - number2;
+        int multiplicacion = number1 * number2;
+        double division = number1 / number2;
+
+        System.out.println(suma);
+        System.out.println(resta);
+        System.out.println(multiplicacion);
+        System.out.println(division);
+
+        // logicos, relacion, comparacion, booleanos
+        /*
+        mayor que >
+        menor que <
+        mayor que o igual >=
+        menor que o igual <=
+        igual que ==
+        distinto de !=
+        and &&
+        or ||
+        negacion !
+         */
+
+        // asignacion
+        /*
+        igual =
+        operaciones aritmetica mas asignacion
+        suma +=
+        resta -=
+        multiplicacion *=
+        division /=
+        resto %=
+         */
+
+        // incremento ++
+        // decremento --
+        // concatenacion en String +
+    }
+}
