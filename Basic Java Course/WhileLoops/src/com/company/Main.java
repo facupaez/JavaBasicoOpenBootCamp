e3073d02b42cdaa19ecf022e11315ec3b54f7c87
facupaez <facu.pz10@gmail.com>
2023-04-03 22:57:39 -0300

    while loops

---
+++ Basic Java Course/WhileLoops/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        // si la condicion es veradera entra en el loop, cuando es falsa nunca entra al loop
+        while(count < 10){
+            System.out.println("Hola, valgo " + count);
+            count++;
+        }
+    }
+}
