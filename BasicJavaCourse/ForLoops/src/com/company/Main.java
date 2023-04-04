024dd5b04160826facda583f930728e86dddfe28
facupaez <facu.pz10@gmail.com>
2023-04-03 22:42:15 -0300

    for loops

---
+++ BasicJavaCourse/ForLoops/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //los bucles for nos permite repetir codigo la cantidad de veces que le indiquemos
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Esta es mi " + i + " iteración.");
+        }
+    }
+}
