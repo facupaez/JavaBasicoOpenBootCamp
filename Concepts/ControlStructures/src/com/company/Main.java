a31f351201df0159053b54e1929c4b224eb8f362
facupaez <facu.pz10@gmail.com>
2023-04-08 15:40:32 -0300

    if...else if - switch sentences

--- Concepts/ControlStructures/src/com/company/Main.java
+++ Concepts/ControlStructures/src/com/company/Main.java
@@ -10,15 +10,50 @@ public class Main {
         int num3 = 20;
 
         // estructura if
-        if(check){
+        if (check) {
             System.out.println("Es verdadero");
         }
 
         // estructura if..else
-        if(num1 < num2 && num2 < num3){
+        if (num1 < num2 && num2 < num3) {
             System.out.println("Es verdadero");
-        }else{
+        } else {
             System.out.println("Es falso");
+        }
+
+        // estructura if...else if
+
+        String dia = "Martes";
+
+        if (dia.equals("Lunes")) {
+            System.out.println("Hoy es lunes usando if...else if!");
+        } else if (dia.equals("Martes")) {
+            System.out.println("Hoy es martes if...else if!");
+        } else if (dia.equals("Domingo")) {
+            System.out.println("Hoy es domingo if...else if!");
+        } else {
+            System.out.println("Ingrese un día");
+        }
+
+        // estructura switch
+
+        String estacion = "Verano";
+
+        switch (estacion) {
+            case "Verano":
+                System.out.println("La estación es verano usando switch!");
+                break;
+            case "Otonio":
+                System.out.println("La estación es otoñoo usando switch!");
+                break;
+            case "Invierno":
+                System.out.println("La estación es invierno usando switch!");
+                break;
+            case "Primavera":
+                System.out.println("La estación es primavera usando switch!");
+                break;
+            default:
+                System.out.println("Ingrese una estación");
         }
     }
 }
