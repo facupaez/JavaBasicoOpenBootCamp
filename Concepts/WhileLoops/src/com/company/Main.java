b21d7632bb33b237cc5f6338c19cd0e661e23cee
facupaez <facu.pz10@gmail.com>
2023-04-08 16:34:36 -0300

    use of break and continue in loops

--- Concepts/WhileLoops/src/com/company/Main.java
+++ Concepts/WhileLoops/src/com/company/Main.java
@@ -6,9 +6,41 @@ public class Main {
 
         int count = 0;
         // si la condicion es veradera entra en el loop, cuando es falsa nunca entra al loop
-        while(count < 10){
+        // mientras la condicion es verdadera se ejecuta el loop, cuando es falsa se detiene el loop
+        while (count < 10) {
             System.out.println("Hola, valgo " + count);
             count++;
+        }
+
+        System.out.println("\nUso de while:\n");
+        //probando while con un array
+        int[] numbers = {1, 2, 3, 4, 5};
+        int contador = 0;
+        while (contador < numbers.length) {
+            System.out.println(numbers[contador]);
+            contador++;
+        }
+
+        System.out.println("\nUso break:\n");
+        // para romper un bucle podemos utilizar la palabra reservada break;
+        contador = 0;
+        while (contador < numbers.length) {
+            if (numbers[contador] == 3) {
+                break;
+            }
+            System.out.println(numbers[contador]);
+            contador++;
+        }
+
+        System.out.println("\nUso continue:\n");
+        // para saltar una iteracion en un bucle podemos utilizar la palabra reservada continue;
+        contador = -1;
+        while (contador < numbers.length - 1) {
+            contador++;
+            if (numbers[contador] == 3) {
+                continue;
+            }
+            System.out.println(numbers[contador]);
         }
     }
 }
