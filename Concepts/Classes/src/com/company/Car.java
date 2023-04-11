2fae9258707e0bd8578af4bbd7b62e3e92448c4f
facupaez <facu.pz10@gmail.com>
2023-04-10 22:20:44 -0300

    add comments to constructors Car class

--- Concepts/Classes/src/com/company/Car.java
+++ Concepts/Classes/src/com/company/Car.java
@@ -1,13 +1,13 @@
 package com.company;
 
-    /*
-    podemos declarar una clase padre com abstracta para poder definir una serie atributos, constructores
-    y metodos pero que solo se puedan instanciar por medio de las clases hijas
-     */
+/*
+podemos declarar una clase padre com abstracta para poder definir una serie atributos, constructores
+y metodos pero que solo se puedan instanciar por medio de las clases hijas
+ */
 public abstract class Car {
     // usamos clases para representar objetos de la vida real
 
-    // atributos/propiedades = caracteristicas del objeto
+    // 1. atributos/propiedades =  caracteristicas del objeto
     String color;
     String manufacturer;
     String brand;
@@ -17,11 +17,12 @@ public abstract class Car {
     double length;
     int velocity = 0;
 
-    // constructores = nos permite representar objetos de esta clase
+    // 2. constructores = nos permite representar objetos de esta clase
     // por defecto se crea un constructor vacio sin valores en los atributos
     public Car() { // por defecto
     }
 
+    // sobre carga de constructor
     // constructor personalizado con todos sus atributos recibidos como argumentos
     public Car(String color, String manufacturer, String brand, int doorsNum, int wheelsNum, double weight, double length) {
         this.color = color;
@@ -33,13 +34,14 @@ public abstract class Car {
         this.length = length;
     }
 
+    // sobre carga de constructor
     // constructor personalizado con 2 atributos recibidos como argumentos
     public Car(String color, String manufacturer) {
         this.color = color;
         this.manufacturer = manufacturer;
     }
 
-    // comportamientos del objeto = funciones/metodos
+    // 3. comportamientos del objeto = funciones/metodos
 
     public void speedUp(int amount) {
         if (amount > 0 && amount <= 180) {
