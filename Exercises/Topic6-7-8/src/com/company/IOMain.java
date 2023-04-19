0a080f1d715345c734458763e3e92f46f3b283c1
facupaez <facu.pz10@gmail.com>
2023-04-18 22:16:35 -0300

    topic 7-8-9: working with InputStream & PrintStream

---
+++ Exercises/Topic6-7-8/src/com/company/IOMain.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.io.*;
+
+/**
+ * @author Facu Paez
+ * 8. Utilizando InputStream y PrintStream, crea una función que reciba dos parámetros: "fileIn" y "fileOut".
+ * La tarea de la función será realizar la copia del fichero dado en el parámetro "fileIn" al fichero
+ * dado en "fileOut".
+ * @version 1.0
+ */
+public class IOMain {
+
+    public static void main(String[] args) {
+
+        try {
+            InputStream from = new FileInputStream("D:\\Programacion\\Ejercicios OpenBootcamp\\Java basico\\Concepts\\StandarIO\\from.txt");
+            PrintStream to = new PrintStream("D:\\Programacion\\Ejercicios OpenBootcamp\\Java basico\\Concepts\\StandarIO\\to.txt");
+
+            try {
+                copyFile(from, to);
+                System.out.println("File copied successfully!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void copyFile(InputStream fileIn, PrintStream fileOut) throws IOException {
+
+        try {
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = fileIn.read(buffer)) != -1) {
+                fileOut.write(buffer, 0, len);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
