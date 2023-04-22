361ecbee99d8b6a1f6fb15efcfa1994c35d81fe4
facupaez <facu.pz10@gmail.com>
2023-04-21 23:11:51 -0300

    desktop event handling

--- Concepts/JavaSwing/src/desktop/MainForm.java
+++ Concepts/JavaSwing/src/desktop/MainForm.java
@@ -1,6 +1,10 @@
 package desktop;
 
+import com.sun.tools.javac.Main;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm {
     private JPanel taxPanel;
@@ -8,6 +12,22 @@ public class MainForm {
     private JTextPane txt_tax;
     private JTextPane txt_total;
     private JButton calculateBtn;
+
+
+
+    public MainForm() {
+        // creando event listener
+        calculateBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double price = Double.parseDouble(txt_price.getText());
+                double tax = Double.parseDouble(txt_tax.getText());
+
+                double result = price + (price / 100 * tax);
+                txt_total.setText(Double.toString(result));
+            }
+        });
+    }
 
     public static void main(String[] args) {
 
